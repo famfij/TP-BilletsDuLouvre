53be7ecffa91c5f1cf6181fdcc5075b5540f15a0
Jean Friry <friryj@gmail.com>
2015-12-04 23:48:08 +0100

    create the Holidays class and tests

--- src/Ufib/HolidaysBundle/Holidays/Holidays.php
+++ src/Ufib/HolidaysBundle/Holidays/Holidays.php
@@ -9,7 +9,105 @@
 namespace Ufib\HolidaysBundle\Holidays;
 
 
+use DateTime;
+
 class Holidays
 {
+    protected $fixedHolidays = Array('01-01', '05-01', '05-08', '07-14', '08-15', '11-01', '11-11', '12-25');
 
+    /**
+     * @param DateTime $day
+     * @return bool
+     */
+    public function isNotWorkedDay(DateTime $day)
+    {
+       return $this->isWeekend($day) || $this->isDayOff($day);
+    }
+
+    /**
+     * @param DateTime $day
+     * @return bool
+     */
+    public function isWeekend(DateTime $day)
+    {
+        $weekDay = date('w', $day->getTimestamp());
+
+        return ($weekDay == 0 || $weekDay == 6);
+    }
+
+    /**
+     * @param DateTime $day
+     * @return bool
+     */
+    protected function isDayOff(DateTime $day)
+    {
+       return $this->isEasterMondayDate($day)
+            || $this->isAscensionDate($day)
+            || $this->isPentecoteMondayDate($day)
+            || $this->isFixedHolidays($day);
+    }
+
+    /**
+     * @param DateTime $day
+     * @return bool
+     */
+    protected function isEasterMondayDate(DateTime $day)
+    {
+        $interval = 'P1D';
+        return $this->isEasterDayPlusInterval($day, $interval);
+    }
+
+    /**
+     * @param DateTime $day
+     * @return bool
+     */
+    protected function isAscensionDate(DateTime $day)
+    {
+        $interval = 'P39D';
+        return $this->isEasterDayPlusInterval($day, $interval);
+    }
+
+    /**
+     * @param DateTime $day
+     * @return bool
+     */
+    protected function isPentecoteMondayDate(DateTime $day)
+    {
+        $interval = 'P50D';
+        return $this->isEasterDayPlusInterval($day, $interval);
+    }
+
+    /**
+     * @param DateTime $day
+     */
+    private function isFixedHolidays(DateTime $day)
+    {
+        $isDayOff = false;
+        $extractedMonthAndDay = $day->format('m-d');
+        foreach ($this->fixedHolidays as $monthAndDay) {
+            if ($monthAndDay == $extractedMonthAndDay) {
+                $isDayOff = true;
+                break;
+            }
+        }
+
+        return $isDayOff;
+    }
+
+    /**
+     * @param DateTime $day
+     * @param string $interval
+     * @return bool
+     */
+    private function isEasterDayPlusInterval(DateTime $day, $interval)
+    {
+        $year = date('Y', $day->getTimestamp());
+        $monthAndDay = $day->format('m-d');
+        $easter = easter_date($year);
+        $testedDate = new DateTime();
+        $testedDate->setTimestamp($easter);
+        $testedDate->add(new \DateInterval($interval));
+
+        return $monthAndDay == $testedDate->format('m-d');
+    }
 }
--- src/Ufib/HolidaysBundle/Tests/Holidays/HolidaysTest.php
+++ src/Ufib/HolidaysBundle/Tests/Holidays/HolidaysTest.php
@@ -9,7 +9,87 @@
 namespace Ufib\HolidaysBundle\Tests\Holidays;
 
 
+use Ufib\HolidaysBundle\Holidays\Holidays;
+
 class HolidaysTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  Holidays $holidays */
+    protected static $holidays;
 
+    public static function setUpBeforeClass()
+    {
+        self::$holidays = new Holidays();
+    }
+
+    public function testIsNotWorkedDay()
+    {
+        $testedValues = array(
+            array('day' => 1, 'month' => 1, 'year' => 2016, 'result' => true),
+            array('day' => 1, 'month' => 5, 'year' => 2016, 'result' => true),
+            array('day' => 1, 'month' => 5, 'year' => 2017, 'result' => true),
+            array('day' => 8, 'month' => 5, 'year' => 2020, 'result' => true),
+            array('day' => 14, 'month' => 7, 'year' => 2020, 'result' => true),
+            array('day' => 15, 'month' => 8, 'year' => 2023, 'result' => true),
+            array('day' => 1, 'month' => 11, 'year' => 2018, 'result' => true),
+            array('day' => 11, 'month' => 11, 'year' => 2020, 'result' => true),
+            array('day' => 25, 'month' => 12, 'year' => 2023, 'result' => true),
+            array('day' => 5, 'month' => 12, 'year' => 2015, 'result' => true),
+            array('day' => 6, 'month' => 3, 'year' => 2016, 'result' => true),
+            array('day' => 14, 'month' => 3, 'year' => 2016, 'result' => false),
+            array('day' => 6, 'month' => 4, 'year' => 2015, 'result' => true),
+            array('day' => 14, 'month' => 5, 'year' => 2015, 'result' => true),
+            array('day' => 25, 'month' => 5, 'year' => 2015, 'result' => true),
+            array('day' => 2, 'month' => 4, 'year' => 2018, 'result' => true),
+            array('day' => 10, 'month' => 5, 'year' => 2018, 'result' => true),
+            array('day' => 21, 'month' => 5, 'year' => 2018, 'result' => true),
+            array('day' => 18, 'month' => 4, 'year' => 2022, 'result' => true),
+            array('day' => 26, 'month' => 5, 'year' => 2022, 'result' => true),
+            array('day' => 6, 'month' => 6, 'year' => 2022, 'result' => true),
+            array('day' => 29, 'month' => 3, 'year' => 2032, 'result' => true),
+            array('day' => 6, 'month' => 5, 'year' => 2032, 'result' => true),
+            array('day' => 17, 'month' => 5, 'year' => 2032, 'result' => true),
+        );
+
+        $testedDate = new \DateTime();
+        foreach ($testedValues as $record) {
+            $testedDate->setDate($record['year'], $record['month'], $record['day']);
+            $this->assertEquals($record['result'], self::$holidays->isNotWorkedDay($testedDate));
+        }
+    }
+
+    public function testIsWeekend()
+    {
+        $testedValues = array(
+            array('day' => 6, 'month' => 11, 'year' => 2015, 'result' => false),
+            array('day' => 7, 'month' => 11, 'year' => 2015, 'result' => true),
+            array('day' => 6, 'month' => 3, 'year' => 2016, 'result' => true),
+            array('day' => 14, 'month' => 3, 'year' => 2016, 'result' => false),
+            array('day' => 1, 'month' => 6, 'year' => 2016, 'result' => false),
+            array('day' => 18, 'month' => 6, 'year' => 2016, 'result' => true),
+            array('day' => 10, 'month' => 1, 'year' => 2017, 'result' => false),
+            array('day' => 1, 'month' => 1, 'year' => 2017, 'result' => true),
+            array('day' => 28, 'month' => 2, 'year' => 2017, 'result' => false),
+            array('day' => 25, 'month' => 2, 'year' => 2017, 'result' => true),
+            array('day' => 1, 'month' => 4, 'year' => 2018, 'result' => true),
+            array('day' => 26, 'month' => 4, 'year' => 2018, 'result' => false),
+            array('day' => 30, 'month' => 9, 'year' => 2018, 'result' => true),
+            array('day' => 3, 'month' => 9, 'year' => 2018, 'result' => false),
+            array('day' => 2, 'month' => 5, 'year' => 2020, 'result' => true),
+            array('day' => 20, 'month' => 5, 'year' => 2020, 'result' => false),
+            array('day' => 31, 'month' => 10, 'year' => 2020, 'result' => true),
+            array('day' => 9, 'month' => 10, 'year' => 2020, 'result' => false),
+            array('day' => 9, 'month' => 7, 'year' => 2022, 'result' => true),
+            array('day' => 11, 'month' => 7, 'year' => 2022, 'result' => false),
+            array('day' => 8, 'month' => 12, 'year' => 2022, 'result' => false),
+            array('day' => 31, 'month' => 12, 'year' => 2022, 'result' => true),
+            array('day' => 15, 'month' => 8, 'year' => 2023, 'result' => false),
+            array('day' => 6, 'month' => 8, 'year' => 2023, 'result' => true),
+        );
+
+        $testedDate = new \DateTime();
+        foreach ($testedValues as $record) {
+            $testedDate->setDate($record['year'], $record['month'], $record['day']);
+            $this->assertEquals($record['result'], self::$holidays->isWeekend($testedDate));
+        }
+    }
 }
